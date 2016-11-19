fa0a633094578f4ef2a0f26db1f708f129b9e77b
RootHolic <wudi@proqod.com>
2016-11-19 09:46:58 +0800

    oauth: adjust different provider to different callback url

--- forth_ai_backend/oauth/views.py
+++ forth_ai_backend/oauth/views.py
@@ -1,9 +1,10 @@
 from django.contrib.auth.models import User
+from django.http import HttpResponse, HttpResponseRedirect
+
 from rest_auth.registration.views import SocialLoginView
 from allauth.socialaccount.providers.facebook.views import FacebookOAuth2Adapter
 from allauth.socialaccount.providers.github.views import GitHubOAuth2Adapter
 from allauth.socialaccount.providers.oauth2.client import OAuth2Client
-from django.http import HttpResponse, HttpResponseRedirect
 from allauth.socialaccount.models import SocialToken, SocialAccount
 class FacebookLogin(SocialLoginView):
     adapter_class = FacebookOAuth2Adapter
@@ -19,5 +20,8 @@ def detail(request):
 	user = request.user
 	provider = str(SocialAccount.objects.filter(user = user)[0].provider)
 	access =  str(SocialToken.objects.filter(account__user = user)[0])
-	url = 'http://localhost:3001/login?access=%s & provider=%s'%(access, provider)
+	if provider == 'facebook':
+		url = 'http://localhost:3001/facebook/login?access=%s & provider=%s'%(access, provider)
+	elif provider == 'github':
+		url = 'http://localhost:3001/github/login?access=%s & provider=%s'%(access, provider)
 	return HttpResponseRedirect(url)
