7c35e4e721924257dbfa841d73fd9c8c5ce0ac0c
GitHubor2015 <wudi@proqod.com>
2016-11-19 14:09:10 +0800

    oauth: change to return jwt directly

--- forth_ai_backend/oauth/views.py
+++ forth_ai_backend/oauth/views.py
@@ -6,6 +6,9 @@ from allauth.socialaccount.providers.facebook.views import FacebookOAuth2Adapter
 from allauth.socialaccount.providers.github.views import GitHubOAuth2Adapter
 from allauth.socialaccount.providers.oauth2.client import OAuth2Client
 from allauth.socialaccount.models import SocialToken, SocialAccount
+
+import requests, json
+
 class FacebookLogin(SocialLoginView):
     adapter_class = FacebookOAuth2Adapter
     client_class = OAuth2Client
@@ -16,20 +19,27 @@ class GithubLogin(SocialLoginView):
     client_class = OAuth2Client
     callback_url = 'localhost:3001'
 
+class CreateUser(SocialLoginView):
+	pass
+
+
+def login_token(provider, access):
+	url = 'http://localhost:8000/rest-auth/%s/' % provider
+	data = {
+		"access_token": access
+	}
+	response = requests.post(url,data)
+	return response.content
+
+
 def detail(request):
 	user = request.user
+	url = 'http://localhost:3001'
 	provider = str(SocialAccount.objects.filter(user = user)[0].provider)
 	access =  str(SocialToken.objects.filter(account__user = user)[0])
 
-	if provider == 'facebook':
-		url = 'http://localhost:3001/facebook/login?access=%s & provider=%s'%(access, provider)
-	elif provider == 'github':
-		url = 'http://localhost:3001/github/login?access=%s & provider=%s'%(access, provider)
+	detail = json.loads(login_token(provider, access))
 
-	response = HttpResponseRedirect(url)
-	response.set_cookie('provider', provider)
-	response.set_cookie('access', access)
-	response['provider'] = provider
-	response['access'] = access
-
+	response = HttpResponseRedirect(url, detail['token'])
+	# token is response.content
 	return response
--- forth_ai_backend/urls.py
+++ forth_ai_backend/urls.py
@@ -21,6 +21,7 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^accounts/', include('allauth.urls')),
     url(r'^rest-auth/', include('rest_auth.urls')),
+    url(r'^rest-auth/create/', CreateUser.as_view(), name = 'create_user'),
     url(r'^rest-auth/registration/', include('rest_auth.registration.urls')),
     url(r'^rest-auth/facebook/$', FacebookLogin.as_view(), name='fb_login'),
     url(r'^rest-auth/github/$', GithubLogin.as_view(), name='github_login'),
