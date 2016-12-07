24dcc8f6b2156fba106f5e7f6ec4d6a50ee3492f
GitHubor2015 <wudi@proqod.com>
2016-12-07 17:55:50 +0800

    facebook post: can post data to facebook user

--- apps/posts/views.py
+++ apps/posts/views.py
@@ -3,6 +3,8 @@ from rest_framework import viewsets, authentication, permissions, filters
 from .models import *
 from .serializers import *
 
+import requests
+from allauth.socialaccount.models import SocialToken
 
 class DefaultsMixin(object):
 
@@ -10,13 +12,10 @@ class DefaultsMixin(object):
     Default settings for view auth, permissions,
     filtering and pagination
     """
-    authentication_classes = (
-        authentication.BasicAuthentication,
-        authentication.TokenAuthentication,
-    )
+
     permission_classes = (
-        # permissions.IsAuthenticated,
-        permissions.AllowAny,
+        permissions.IsAuthenticated,
+        #permissions.AllowAny,
     )
     paginate_by = 25
     # paginate_by_param = "owner"
@@ -31,6 +30,29 @@ class DefaultsMixin(object):
 class RepostViewSet(DefaultsMixin, viewsets.ModelViewSet):
     serializer_class = RepostSerializer
     queryset = Post.objects.all()
+
+    def fb_login(self, access, post_data):
+        fb_host = 'https://graph.facebook.com/v2.8'
+        user_post_api = fb_host + '/me/feed'
+        message = post_data.get('content')
+        params = {
+            'message':message,
+            'access_token':access
+    
+        }
+        resp = requests.post(user_post_api, params = params)
+
+
+    def perform_create(self, serializers):
+        # TODO: post to github or fb
+        user = self.request.user
+        data = self.request.data
+        access =  str(SocialToken.objects.filter(account__user = user)[0])
+        # POST NLP-->data
+        post_data = data # temp
+        self.fb_login(access, post_data)
+
+        
     # filter_fields = ['owner', ]
 
     # parser_classes = (MultiPartParser, FormParser,)
