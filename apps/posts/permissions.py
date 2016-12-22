db1fa146a534a51adc127de5aa96550451db3336
GitHubor2015 <wudi@proqod.com>
2016-12-22 11:42:41 +0800

    delete permission

--- apps/posts/permissions.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import permissions
-
-
-class IsOwner(permissions.BasePermission):
-    
-    def has_object_permission(self, request, view, obj):
-    	print '?????', obj.owner, request.user
-        return obj.owner == request.user
